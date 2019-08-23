4367a4d7d4d7f9ca81db681c941ef6144e4a2da1
moonl <347755658>
2019-08-23 14:37:26 +0800

    save

--- application/admin/controller/Member.php
+++ application/admin/controller/Member.php
@@ -10,11 +10,17 @@ namespace app\admin\controller;
 
 use Parsedown;
 use tools\Sysinfo;
+use think\Db;
 
 class Member extends Base
 {
+
+  public $table = 'member';
+
   public function index()
   {
+      $list = Db::name($this->table)->order('id desc')->paginate(10);
+      $this->assign('list',$list);
       return $this->fetch();
   }
 
@@ -25,7 +31,13 @@ class Member extends Base
 
   public function del()
   {
-      return false;
+      $id  = input('get.id','','trim');
+      $ret = Db::name($this->table)->where(['id'=>$id])->delete();
+      if ($ret) {
+         $this->success('删除成功！');
+      }else {
+        $this->error('删除失败');
+      }
   }
 
 }
