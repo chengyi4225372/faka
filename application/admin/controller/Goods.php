285072342ca3b0c7d6839719b5e3cce19b2f93ba
moonl <347755658>
2019-08-26 09:52:29 +0800

    add all

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -10,6 +10,7 @@ namespace app\admin\controller;
 
 use Parsedown;
 use tools\Sysinfo;
+use think\Db;
 
 class Goods extends Base
 {
@@ -17,12 +18,67 @@ class Goods extends Base
     //商品类型
     public function cates()
     {
-        return $this->fetch();
+       if($this->request->isGet()){
+           $keywords = input('get.keywords','','trim');
+           $keywords ? $keywords :'';
+
+           if(!empty($keywords)){
+               $list     = Db::name('good_cates')->where(['title'=>$keywords])->order('id desc')->paginate(15);
+           }else {
+               $list     = Db::name('good_cates')->order('id desc')->paginate(15);
+           }
+
+           $this->assign('list',$list);
+           return $this->fetch();
+       }
+
+       if($this->request->isAjax()){
+           $id    = input('post.id','','int|trim');
+           $title = input('post.title','','trim');
+
+           if(empty($id) || $id == ''){
+               $res = Db::name('good_cates')->insert(['title'=>$title]);
+           }
+
+           if(!empty($id) && isset($id)){
+               $res = Db::name('good_cates')->where(['id'=>$id])->update(['title'=>$title]);
+           }
+       }
+
+        if($res){
+            $this->result('','200','操作成功','json');
+        }else {
+            $this->result('','404','操作失败','json');
+        }
+
     }
 
     public function addc(){
+        if($this->request->isGet()){
+             $id = input('get.id','','trim');
+             if(!empty($id) || !isset($id)){
+              $info = Db::name('good_cates')->where(['id'=>$id])->find();
+              $this->assign('info',$info);
+             }
+        }
         return $this->fetch();
     }
+
+
+    public function delc()
+    {
+        if($this->request->isGet()){
+            $id  = input('get.id','','int');
+            $res = Db::name('good_cates')->where(['id'=>$id])->delete();
+
+            if($res){
+                $this->success('操作成功');
+            }else {
+                $this->error('操作失败');
+            }
+        }
+    }
+
 
 
     //商品列表
