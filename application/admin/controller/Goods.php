c589f873ef87ece0a352a2bc65165375f7282bf3
moonl <347755658>
2019-08-27 13:50:07 +0800

    add

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -86,31 +86,60 @@ class Goods extends Base
     {
       if($this->request->isGet()){
           $keywords = input('get.keywords','','trim');
-          $keywords = $keywords?$keywords:'';
-          if(!isset($keywords) || empty($keywords)){
-              $list = Db::name('goods')->where(['title'=>$keywords])->order(['sort'=>'desc','id'=>'desc'])->paginate(15);
+          if(!isset($keywords) || !empty($keywords)){
+              $list = Db::name('goods')->where(['title'=>$keywords])->order('sort desc,id desc')->paginate(15);
           }else {
-              $list = Db::name('goods')->order(['sort'=>'desc','id'=>'desc'])->paginate(15);
+              $list = Db::name('goods')->order('sort desc ,id desc')->paginate(15);
           }
+          $cates    = Db::name('good_cates')->select();
+          $category = array_column($cates,'title','id');
+          $this->assign('category',$category);
           $this->assign('list',$list);
-          return $this->fetch();
       }
-
+        return $this->fetch();
     }
 
     public function addg()
     {
       if($this->request->isGet()){
-          $id = input('get.id','','int|trim');
-          $info = Db::name('goods')->where(['id'=>$id])->find();
+          $id    = input('get.id','','int');
+          $info  = Db::name('goods')->where(['id'=>$id])->find();
+          $cates = Db::name('good_cates')->order('id desc')->select();
           $this->assign('info',$info);
+          $this->assign('cates',$cates);
           return $this->fetch();
       }
 
 
       if($this->request->isAjax()){
-         return true;
-       }
+          $this->param = $this->request->param();
+
+          //add
+          if(empty($this->param['dat']['id']) || $this->param['dat']['id']<=0 ){
+              $res = Db::name('goods')->insert($this->param['dat']);
+          }
+
+          //edit
+          if(!empty($this->param['dat']['id']) && isset($this->param['dat']['id'])){
+              $res = Db::name('goods')->where(['id'=>$this->param['dat']['id']])->update(
+                  ['title' =>$this->param['dat']['title'],
+                   'sort'  =>$this->param['dat']['sort'],
+                   'num'   =>$this->param['dat']['num'],
+                   'images'  =>$this->param['dat']['images'],
+                   'money'  =>$this->param['dat']['money'],
+                   'content'  =>$this->param['dat']['content'],
+                   'huo'  =>$this->param['dat']['huo'],
+                   'cid'  =>$this->param['dat']['cid'],
+                 ]);
+          }
+
+          if($res){
+              $this->result('','200','操作成功','json');
+          }else{
+              $this->result('','400','操作失败','json');
+          }
+
+      }
 
     }
 
@@ -138,16 +167,48 @@ class Goods extends Base
 
     }
 
-
+   //上传图片
     public function uploads_img()
     {
-        $this->request->file();
+        // 获取上传文件
+        $file =$this->request->file('file');
+        // 验证图片,并移动图片到框架目录下。
+        $path = ROOT_PATH.'public/Upload/imgs/';
+        $info = $file-> move($path);
+        if($info){
+            $mes = $info->getSaveName();
+            $mes = str_replace("\\",'/',$mes);
+            return json(['code'=>'200','msg'=>'上传成功','path'=>'/Upload/imgs/'.$mes]);
+        }else{
+            // 文件上传失败后的错误信息
+            $mes = $file->getError();
+            return json(['code'=>'400','msg'=>$mes]);
+        }
     }
 
-    //排序
-    public function statusg()
+    //商品 上架状态
+    public function jiag()
     {
-        return false;
+       if($this->request->isAjax()){
+           $this->param = $this->request->param();
+           $id = $this->param['id'];
+           $status = $this->param['data'];
+
+           if(empty($id)|| !isset($id)){
+               return false;
+           }
+
+           if(!empty($id) && isset($id)){
+               $ret = Db::name('goods')->where(['id'=>$id])->update(['status'=>$status?'0':'1']);
+           }
+
+           if($ret){
+               $this->result('','200','操作成功','json');
+           }else{
+               $this->result('','400','操作失败','json');
+           }
+
+       }
     }
 
 
