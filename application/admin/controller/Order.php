9770b5d54f626b397fec0c739bcd9168bfd93f23
moonl <347755658>
2019-09-03 15:27:47 +0800

    add

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -12,6 +12,7 @@ namespace app\admin\controller;
 use Parsedown;
 use tools\Sysinfo;
 use Think\Db;
+use Think\Session;
 
 class Order extends Base
 {  
@@ -19,11 +20,26 @@ class Order extends Base
     //自动发货
     public function index()
     {
-        $list = Db::name('order')->where(['huo'=>0,'is_delete'=>0])->paginate(15);
-        $goods =Db::name('goods')->where('huo',0)->select();
-        $goods = array_column($goods,'title','id');
+        $gid     = input('get.gid','','int');
+        $paytype = input('get.paytype','','int');
+
+        if(empty($gid) && empty($paytype)){
+            $w = ['huo'=>0, 'is_delete'=>0];
+        }
+
+        if(!empty($gid) && !empty($paytype)){
+            $w = ['huo'=>0,'is_delete'=>0,'gid'=>$gid,'paytype'=>$paytype];
+        }
+
+        $list   = Db::name('order')->where($w)->paginate(15);
+        $goodes = Db::name('goods')->where('huo',0)->select();
+        $member = Db::name('member')->field('id,account')->select();
+        $goods  = array_column($goodes,'title','id');
+        $info   = array_column($member,'account','id');
         $this->assign('list',$list);
         $this->assign('goods',$goods);
+        $this->assign('goodes',$goodes);
+        $this->assign('info',$info);
         return $this->fetch();
     }
 
@@ -56,20 +72,30 @@ class Order extends Base
         }
     }
 
-    //补单
-    public function overorder()
-    {
-     return  111;
-    }
-
     //人工发货
     public function person()
     {
-        $list  = Db::name('order')->where(['huo'=>1,'is_delete'=>0])->paginate(15);
-        $goods =Db::name('goods')->where('huo',1)->select();
-        $goods = array_column($goods,'title','id');
+        $gid     = input('get.gid','','int');
+        $paytype = input('get.paytype','','int');
+
+        if(empty($gid) && empty($paytype)){
+            $w = ['huo'=>1, 'is_delete'=>0];
+        }
+
+        if(!empty($gid) && !empty($paytype)){
+            $w = ['huo'=>1,'is_delete'=>0,'gid'=>$gid,'paytype'=>$paytype];
+        }
+
+
+        $list   = Db::name('order')->where($w)->paginate(15);
+        $goodes = Db::name('goods')->where('huo',1)->select();
+        $goods  = array_column($goodes,'title','id');
+        $member = Db::name('member')->field('id ,account')->select();
+        $info   = array_column($member,'title','id');
         $this->assign('goods',$goods);
+        $this->assign('goodes',$goodes);
         $this->assign('list',$list);
+        $this->assign('info',$info);
         return $this->fetch();
     }
 
@@ -89,7 +115,17 @@ class Order extends Base
 
     public function pdel()
     {
-        return true;
+       $id = input('get.id','','int');
+       if($id <=0 || empty($id)){
+           return false;
+       }
+       $ret = Db::name('order')->where(['id'=>$id])->update(['is_delete'=>1]);
+       if($ret){
+           return json(['code'=>200,'msg'=>'删除成功']);
+       }else {
+           return json(['code'=>400,'msg'=>'删除失败']);
+       }
+
     }
 
     //代充订单
@@ -103,5 +139,11 @@ class Order extends Base
     public function chong()
     {
         return $this->fetch();
+    }
+
+    //补单
+    public function overorder()
+    {
+        return  111;
     }
 }
