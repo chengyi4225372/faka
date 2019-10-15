043acfd3a52fbc5ee563faf17c0df4b26f1fe3a0
moonl <347755658>
2019-10-15 16:40:46 +0800

    add

--- application/index/controller/Mobile.php
+++ application/index/controller/Mobile.php
@@ -3,9 +3,10 @@ namespace app\index\controller;
 
 use think\Config;
 use think\Controller;
-
+use think\Db;
 class Mobile extends Controller
 {
+
     //pc 端控制
     public function _initialize()
     {
@@ -13,16 +14,70 @@ class Mobile extends Controller
         if(!isMobile()){
             $this->redirect('index/index');
         }
+
+        $config = Db::name('config')->where(['id'=>1])->find();
+        $shouye = Db::name('content')->where(['id'=>24])->find();
+        $this->assign('config',$config);
+        $this->assign('shouye',$shouye);
     }
 
+    //主页
     public function index()
     {
-        return $this->fetch();
+       if($this->request->isGet()){
+           $list   = Db::name('goods')->where(['status'=>1])->order('sort desc')->select();
+           $cates  = Db::name('good_cates')->order('id desc')->select();
+           $indexs = Db::name('content')->where(['id'=>24])->find();
+           $categorys = array_column($cates,'title','id');
+           //商品库存
+           $num = Db::name('card')->field('gid,count(kami) as num')->where(['over'=>0])->group('gid')->select();
+
+           foreach ($list as $k =>$val){
+               if(!is_array($num) || empty($num)){
+                   $list[$k]['num'] = null;
+               }
+
+               if(is_array($num) && !empty($num)){
+                   foreach($num as $key =>$v){
+                       if($list[$k]['huo'] == 0 && $list[$k]['id'] == $v['gid']){
+                           $list[$k]['num'] = $v['num'];
+                       }
+                   }
+               }
+
+               if($list[$k]['huo'] == 1){
+                   $list[$k]['num'] = '人工发货';
+               }
+           }
+
+           $this->assign('list',$list);
+           $this->assign('categorys',$categorys);
+           $this->assign('indexs',$indexs);
+           return $this->fetch();
+       }
+    }
+
+    //详情
+    public function trade(){
+        if($this->request->isGet()){
+            $id = input('get.id','','int');
+            if(empty($id) || $id <=0){
+                return false;
+            }
+
+            $info = Db::name('goods')->where('id',$id)->find();
+            $this->assign('info',$info);
+            return $this->fetch();
+        }
+        return false;
     }
 
     public function buy()
     {
-        return $this->fetch();
+       if($this->request->isGet()){
+
+       }
+
     }
 
     public function orderinfo()
@@ -40,10 +95,6 @@ class Mobile extends Controller
         return $this->fetch();
     }
 
-    public function trade()
-    {
-        return $this->fetch();
-    }
 
     public function zdfahuo()
     {
