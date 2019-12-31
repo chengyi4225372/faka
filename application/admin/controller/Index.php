f97698e9779fcddf6228d4cef06908b3b2d8fb27
cheng <732345907@qq.com>
2019-12-31 11:23:17 +0800

    add

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -8,6 +8,7 @@ namespace app\admin\controller;
 
 use Parsedown;
 use tools\Sysinfo;
+use think\Db;
 
 class Index extends Base
 {
@@ -19,9 +20,33 @@ class Index extends Base
 
         $this->assign('zan',$zan);
 
-        $this->assign('html',$html);
+        $this->assign('html',$html); 
+        
+        //会员
+        $member = Db::name('member')->count();
+        //昨天订单
+        $zuo_order = Db::name('order')->where(['create_time'=>date("Y-m-d",strtotime("-1 day"))])->count();
+        //今天订单
+        $today     = Db::name('order')->where(['create_time'=>date("Y-m-d",time())])->count();
+       //昨日订单金额
+        $zuo_money = Db::name('order')
+        ->where(['create_time'=>date("Y-m-d",strtotime("-1 day"))])
+        ->where(['status'=>1])
+        ->count('countpay');
+       //今日金额
+        $today_money= Db::name('order')
+        ->where(['create_time'=>date("Y-m-d",time())])
+        ->where(['status'=>1])
+        ->count('countpay');
+       //未发货订单总数
+       
 
-        return $this->fetch();
+       $this->assign('member',$member);
+       $this->assign('zuo_order',$zuo_order);
+       $this->assign('today',$today);
+       $this->assign('zuo_money',$zuo_money);
+       $this->assign('today_money',$today_money);
+       return $this->fetch();
     }
 
 }
